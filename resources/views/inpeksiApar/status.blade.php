aa72e790eca4a7b5bb7caafbf3a01f80101a5fdd
fauziramdaniii <fauziramdhan38@gmail.com>
2022-02-28 18:01:56 +0700

    sip

--- resources/views/inpeksiApar/status.blade.php
+++ resources/views/inpeksiApar/status.blade.php
@@ -2,29 +2,37 @@
 @section('sidebar')
 @if(Auth::User()->role == 'supervisor')
 <li class="nav-item">
-    <a class="nav-link {{Request::is('apar/dashboard*') ? 'active' : ''}} " href="/apar/dashboard">
+    <a class="nav-link" href="/apar/dashboard">
         <i class="icon-grid menu-icon"></i>
         <span class="menu-title">Dashboard</span>
     </a>
 </li>
-<li class="nav-item {{Request::is('apar/dataapar*') ? 'active' : ''}}">
+<li class="nav-item">
     <a class="nav-link" href="/apar/dataapar">
         <i class="icon-contract menu-icon"></i>
         <span class="menu-title">Master APAR</span>
     </a>
 </li>
-<li class="nav-item {{Request::is('apar/masterinspeksi*') ? 'active' : ''}}">
+<li class="nav-item">
     <a class="nav-link" href="/apar/masterinspeksi">
         <i class="icon-paper menu-icon"></i>
         <span class="menu-title">Master Inspeksi</span>
     </a>
 </li>
 
-<li class="nav-item {{Request::is('apar/inspeksi*') ? 'active' : ''}}">
-    <a class="nav-link" href="/apar/inspeksi">
-        <i class="icon-paper menu-icon"></i>
+<li class="nav-item">
+    <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+        <i class="icon-head menu-icon"></i>
         <span class="menu-title">Inspeksi APAR</span>
+        <i class="menu-arrow"></i>
     </a>
+    <div class="collapse" id="ui-basic">
+        <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href=""> Report APAR</a></li>
+            <li class="nav-item"> <a class="nav-link" href="/apar/statusapar">Status Apar</a></li>
+            <li class="nav-item"> <a class="nav-link" href="/apar/aparinspeksi"> Inspeksi APAR </a></li>
+        </ul>
+    </div>
 </li>
 @else
 <li class="nav-item">
@@ -34,97 +42,114 @@
     </a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" href="/apar/inspeksi">
-        <i class="icon-paper menu-icon"></i>
+    <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+        <i class="icon-layout menu-icon"></i>
         <span class="menu-title">Inspeksi APAR</span>
+        <i class="menu-arrow"></i>
     </a>
+    <div class="collapse" id="ui-basic">
+        <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="#">APAR Report</a></li>
+            <li class="nav-item"> <a class="nav-link" href="/apar/statusapar">Status Apar</a></li>
+            <li class="nav-item"> <a class="nav-link" href="/apar/aparinspeksi">APAR Inspeksi</a></li>
+        </ul>
+    </div>
 </li>
 @endif
 @endsection
 <?php $no = 1 ?>
 @section ("content")
-@include('sweetalert::alert')
-<br>
-<h3 class="font-weight-bold text-center">
-    Data Inpeksi APAR (Periode {{date('F Y',strtotime($aparinspeksi->periode))}})
-</h3>
-
-<div class="col-sm-12">
+<div class="content-wrapper">
     <br>
-    @if (session()->get('success'))
-    <div class="alert alert-sucess">
-        {{ session()->get('sucess') }}
+    <h3>
+        <center>Data Inpeksi APAR (Periode {{date('F Y')}})
+    </h3>
+
+    <div class="col-sm-12">
+        <br>
+        @if (session()->get('success'))
+        <div class="alert alert-sucess">
+            {{ session()->get('sucess') }}
+        </div>
+        @endif
     </div>
-    @endif
-</div>
+    @if(!empty($aparinspeksi))
+    <center>
+        <div class="row">
+            <div class="col-md-6 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">{{$periode->sudah_inspeksi}}</h4>
+                        <div class="media">
 
-<div class="row">
-    <div class="col-md-6 grid-margin stretch-card">
-        <div class="card">
-            <div class="card-body text-center">
-                <h4 class="card-title">{{$sudahInspeksi}}</h4>
-                <div class="media">
+                            <div class="media-body">
+                                <p class="card-text">APAR yang sudah diinpeksi</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">{{$periode->belum_inspeksi}}</h4>
+                        <div class="media">
 
-                    <div class="media-body">
-                        <p class="card-text">APAR yang sudah diinpeksi</p>
+                            <div class="media-body">
+                                <p class="card-text">APAR yang belum diinpeksi</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-6 grid-margin stretch-card">
-        <div class="card">
-            <div class="card-body text-center">
-                <h4 class="card-title">{{$belumInspeksi}}</h4>
-                <div class="media">
+        <div class="table-responsive">
+            <table class="display expandable-table col-sm-12">
+                <thead>
+                    <tr class="text-center">
+                        <th> No </th>
+                        <th> Kode Apar </th>
+                        <th> Tipe Apar </th>
+                        <th> Jenis Apar </th>
+                        <th>
+                            Status
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($aparinspeksi as $dataapar)
+                    <tr class="text-center">
+                        <td> {{ $no++ }} </td>
+                        <td> {{ $dataapar->Apart->id }} </td>
+                        <td> {{ $dataapar->Apart->tipe }} </td>
+                        <td> {{ $dataapar->Apart->jenis }} </td>
+                        <td>
+                            @if($dataapar->jenis==null)
+                            <p class="text-danger">Apar Belum Diinpeksi</p>
+                            @else
+                            <a class="text-success lihatHasil" href="#" data-toggle="modal" data-target="#modalHasil">Apar Sudah Diinpeksi</a>
+                            @endif
+                        </td>
 
-                    <div class="media-body">
-                        <p class="card-text">APAR yang belum diinpeksi</p>
-                    </div>
-                </div>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        @else
+        <div class="card">
+            <div class="card-body">
+                <h4 class="text-center">Inpeksi Apart Bulan {{date('F Y')}} Belum Dibuka</h4>
             </div>
         </div>
-    </div>
-</div>
-<div class="table-responsive">
-    <a href="/apar/inspeksi/{{$aparinspeksi->id}}/inputInpeksiApar" class="btn btn-info btn-md my-2" style="float:right">Input Inspeksi</a>
-    <table class="display expandable-table col-sm-12">
-        <thead>
-            <tr class="text-center">
-                <th> No </th>
-                <th> Kode Apar </th>
-                <th> Tipe Apar </th>
-                <th> Jenis Apar </th>
-                <th>
-                    Status
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($aparinspeksi->DetailInspeksi as $dataapar)
-            <tr class="text-center">
-                <td> {{ $no++ }} </td>
-                <td> {{ $dataapar->Apart->id }} </td>
-                <td> {{ $dataapar->Apart->tipe }} </td>
-                <td> {{ $dataapar->Apart->jenis }} </td>
-                <td>
-                    @if($dataapar->jenis==null)
-                    <p class="text-danger">Apar Belum Diinpeksi</p>
-                    @else
-                    <a class="text-success lihatHasil" href="#" data-toggle="modal" data-target="#modalHasil" data-id="{{$dataapar->id}}">Apar Sudah Diinpeksi</a>
-                    @endif
-                </td>
 
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+        @endif
 </div>
 @endsection
 
 @section('modal')
 <div class="modal fade" id="modalHasil" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-md" role="document">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Hasil Inspeksi</h5>
@@ -133,55 +158,38 @@
                 </button>
             </div>
             <div class="modal-body">
-                <p class="font-weight-bold">Kode APAR : <span id="kdApar"></span></p>
-                <p class="font-weight-bold">Tanggal Inspeksi : <span id="tanggal"></span></p>
-                <table class=" table table-bordered" style="width: 100%">
+                <table class="display expandable-table" style="width: 100%">
+                    <thead>
+                        <tr class="text-center">
+                            <th> PRESSURE/CATRIDGE</th>
+                            <th> NOZZLE </th>
+                            <th> SELANG </th>
+                            <th> TABUNG </th>
+                            <th> RAMBU </th>
+                            <th> LABEL </th>
+                            <th> KONDISI CAT </th>
+                            <th> SAFETY PIN </th>
+                            <th> RODA </th>
+                            <th> KETERANGAN </th>
+                            <th> FOTO </th>
+                        </tr>
+                    </thead>
                     <tbody>
-                        <tr class="">
-                            <td class="font-weight-bold"><b>PRESSURE/CATRIDGE</b> </td>
-                            <td> <span id="pressure"></span></td>
+                        <tr class="text-center">
+                            <td> {{ }} </td>
+                            <td></td>
+                            <td> </td>
+                            <td></td>
+                            <td> </td>
+                            <td> </td>
+                            <td></td>
+                            <td></td>
+                            <td> </td>
+                            <td></td>
+                            <td></td>
                         </tr>
-                        <tr class="">
-                            <td class="font-weight-bold"><b>NOZZLE</b></td>
-                            <td> <span id="nozzle"></span></td>
-                        </tr>
-                        <tr class="">
-                            <td class="font-weight-bold">SELANG</td>
-                            <td> <span id="selang"></span></td>
-                        </tr>
-                        <tr class="">
-                            <td class="font-weight-bold">TABUNG</td>
-                            <td> <span id="tabung"></span></td>
-                        </tr>
-                        <tr class="">
-                            <td class="font-weight-bold">RAMBU</td>
-                            <td> <span id="rambu"></span></td>
-                        </tr>
-                        <tr class="">
-                            <td class="font-weight-bold">LABEL</td>
-                            <td> <span id="label"></span></td>
-                        </tr>
-                        <tr class="">
-                            <td class="font-weight-bold">KONDISI CAT</td>
-                            <td> <span id="cat"></span></td>
-                        </tr>
-                        <tr class="">
-                            <td class="font-weight-bold">SAFETY PIN</td>
-                            <td> <span id="pin"></span></td>
-                        </tr>
-                        <tr class="">
-                            <td class="font-weight-bold">RODA</td>
-                            <td> <span id="roda"></span></td>
-                        </tr>
-                        <tr class="">
-                            <td class="font-weight-bold">KETERANGAN </td>
-                            <td> <span id="keterangan"></span></td>
-                        </tr>
-
                     </tbody>
-
                 </table>
-                <p class="mt-3 font-weight-bold">Bukti Foto :</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -196,23 +204,5 @@
     $(document).on('ready', function() {
         console.log('aa');
     })
-    $('.lihatHasil').on('click', function() {
-        var id = $(this).attr('data-id');
-        $.get('/apar/hasilInspeksi/' + id, function(data) {
-            $('#pressure').text(data.data.jenis);
-            $('#nozzle').text(data.data.noozle);
-            $('#selang').text(data.data.selang);
-            $('#tabung').text(data.data.tabung);
-            $('#rambu').text(data.data.rambu);
-            $('#label').text(data.data.label);
-            $('#cat').text(data.data.cat);
-            $('#pin').text(data.data.pin);
-            $('#roda').text(data.data.roda);
-            $('#keterangan').text(data.data.keterangan);
-            $('#tanggal').text(data.data.tanggal);
-            $('#kdApar').text(data.apar.id);
-
-        });
-    });
 </script>
 @endsection
